9faf3df827a4ce6268625dcd97ae864156f37506
Pallavi <pallavibobde1@gmail.com>
2021-06-30 11:14:50 +0530

    second commit

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let hamburger = document.querySelector('.fa-bars');
 let cross = document.querySelector('.fa-times');
 let menu = document.querySelector('.menu');
 hamburger.onclick = () =>{
-    menu.style.right = '0';
+    menu.style.left = '0';
     cross.classList.remove('hide');
     hamburger.classList.add('hide');
 }
 cross.onclick = () =>{
-    menu.style.right = '-100vw';
+    menu.style.left = '100vw';
     cross.classList.add('hide');
     hamburger.classList.remove('hide');
 }
